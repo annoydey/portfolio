d540d7cd41a73035c4f4a506bf2be61efa650fdc
annoydey <annoydey@gmail.com>
2023-11-24 06:48:42 -0500

    Updated - November 2023

--- js/js-script.js
+++ js/js-script.js
@@ -78,7 +78,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: [" Programmer", "MERN Stack developer", "web designer", "android developer"],
+    strings: [" Programmer", "MERN Stack developer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
